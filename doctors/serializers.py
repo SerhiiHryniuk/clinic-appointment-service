dbcf9cd8d5dc4998f933b7898e903d85f63d4610
Artem <artemradckevich@gmail.com>
2026-06-15 17:29:31 +0300

    feat: add doctor slot serializers

--- doctors/serializers.py
+++ doctors/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from doctors.models import Doctor
+from doctors.models import Doctor, DoctorSlot
 from specializations.models import Specialization
 from specializations.serializers import SpecializationSerializer
 
@@ -35,3 +35,63 @@ class DoctorDetailSerializer(serializers.ModelSerializer):
             "specializations",
             "price_per_visit",
         ]
+
+
+class DoctorSlotListSerializer(serializers.ModelSerializer):
+    doctor = serializers.StringRelatedField()
+
+    class Meta:
+        model = DoctorSlot
+        fields = [
+            "id",
+            "doctor",
+            "start",
+            "end",
+        ]
+
+
+class DoctorSlotDetailSerializer(serializers.ModelSerializer):
+    doctor = DoctorListSerializer()
+
+    class Meta:
+        model = DoctorSlot
+        fields = [
+            "id",
+            "doctor",
+            "start",
+            "end",
+        ]
+
+
+class DoctorSlotCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DoctorSlot
+        fields = [
+            "doctor",
+            "start",
+            "end"
+        ]
+
+    def validate(self, data):
+        if data["end"] <= data["start"]:
+            raise serializers.ValidationError(
+                {"end": "End time must be later than start time"}
+            )
+        return data
+
+
+class DoctorSlotBulkCreateSerializer(serializers.Serializer):
+    start = serializers.DateTimeField()
+    end = serializers.DateTimeField()
+    interval = serializers.IntegerField(default=30)
+
+    def validate(self, data):
+        if data["end"] <= data["start"]:
+            raise serializers.ValidationError(
+                {"end": "End time must be later than start time"}
+            )
+        if data.get("interval", 30) <= 0:
+            raise serializers.ValidationError(
+                {"interval": "Interval must be a positive integer"}
+            )
+        return data
