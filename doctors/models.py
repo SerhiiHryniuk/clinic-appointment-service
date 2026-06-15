b26a1c40ab2bb48c2800e5bcd5f71af744bde102
Artem <artemradckevich@gmail.com>
2026-06-15 15:17:30 +0300

    feat: add doctor slot model

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from specializations.models import Specialization
@@ -17,3 +18,34 @@ class Doctor(models.Model):
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
+
+
+class DoctorSlot(models.Model):
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
+        related_name="slots"
+    )
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["doctor", "start", "end"],
+                name="unique_doctor_slot"
+            )
+        ]
+
+    def clean(self):
+        if self.end <= self.start:
+            raise ValidationError(
+                {"end": "End time must be later than start time"}
+            )
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.start:%d.%m.%Y %H:%M} - {self.end:%H:%M}"
