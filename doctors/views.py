ee8ecffbd51e5fd4608cbc6afb35969c6bcdd643
Artem <artemradckevich@gmail.com>
2026-06-15 17:31:06 +0300

    feat: add doctor view set with filtering

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.db.models import Q
+from rest_framework import viewsets
 
-# Create your views here.
+from config.permissions import IsAdminOrReadOnly
+from doctors.models import Doctor
+from doctors.serializers import (
+    DoctorListSerializer,
+    DoctorDetailSerializer,
+)
+
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        queryset = Doctor.objects.prefetch_related("specializations")
+        specialization = self.request.query_params.get("specialization")
+        if specialization:
+            q = Q(specializations__code=specialization)
+            if specialization.isdigit():
+                q |= Q(specializations__id=specialization)
+            queryset = queryset.filter(q).distinct()
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return DoctorDetailSerializer
+
+        return DoctorListSerializer
