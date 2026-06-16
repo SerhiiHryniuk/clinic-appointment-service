c2f26b55975198f160d115e3aa38675ac0b530b8
Roman Smyk <smyk_roman@ukr.net>
2026-06-16 18:56:42 +0300

    feat: add payments app

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("api/v1/", include("specializations.urls", namespace="specializations")),
     path("api/v1/", include("doctors.urls", namespace="doctors")),
     path("api/v1/", include("appointment.urls", namespace="appointment")),
+    path("api/v1/users/", include("users.urls", namespace="users")),
+    path("api/v1/", include("payments.urls", namespace="payments")),
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path("api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="api-schema"), name="api-docs"),
-    path("api/v1/users/", include("users.urls", namespace="users")),
 ]
